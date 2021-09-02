74f578b13c1c9fc36acfbda61f63694b0a054dbb
agsoto02 <alejandrogarciasoto02@gmail.com>
2021-09-02 19:59:05 -0400

    idk

---
+++ core/src/com/garcia/spaceshooter/SpaceshooterGame.java
@@ -0,0 +1,10 @@
+package com.garcia.spaceshooter;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class SpaceshooterGame extends ApplicationAdapter {
+
+}
---
+++ desktop/src/com/garcia/spaceshooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.garcia.spaceshooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.garcia.spaceshooter.SpaceshooterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceshooterGame(), config);
+	}
+}
